90390311a40cc4a238242f0d5847caf7d53046ea
ambiesoft <ambiesoft.trueff@gmail.com>
2020-04-07 01:21:48 +0900

    2013

--- StdAfx.h
+++ StdAfx.h
@@ -1,7 +1,3 @@
-// stdafx.h : 標準のシステム インクルード ファイル、
-//            または参照回数が多く、かつあまり変更されない
-//            プロジェクト専用のインクルード ファイルを記述します。
-//
 
 #if !defined(AFX_STDAFX_H__D5783068_745A_11D5_8E03_A40A1FE48159__INCLUDED_)
 #define AFX_STDAFX_H__D5783068_745A_11D5_8E03_A40A1FE48159__INCLUDED_
@@ -13,7 +9,7 @@
 #endif // _MSC_VER > 1000
 
 #define VC_EXTRALEAN		// Windows ヘッダーから殆ど使用されないスタッフを除外します。
-
+#include <SDKDDKVer.h>
 #include <afxwin.h>         // MFC のコアおよび標準コンポーネント
 #include <afxext.h>         // MFC の拡張部分
 #include <afxdtctl.h>		// MFC の Internet Explorer 4 コモン コントロール サポート
