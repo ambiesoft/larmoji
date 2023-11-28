6e7da287388aa61b8a5e3fe3a5b0a75639b01234
ambiesoft <ambiesoft.trueff@gmail.com>
2023-11-28 15:37:04 +0900

    ショートカットキーCtrl+Iが動作していなかった問題を修正

--- larmoji/resource.h
+++ larmoji/resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ で生成されたインクルード ファイル。
-// larmoji.rc で使用
+// Microsoft Visual C++ generated include file.
+// Used by larmoji.rc
 //
 #define IDD_ABOUTBOX                    100
 #define IDC_FONTNAME_COMBO              101
@@ -36,7 +36,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
 #define _APS_NEXT_RESOURCE_VALUE        139
-#define _APS_NEXT_COMMAND_VALUE         32783
+#define _APS_NEXT_COMMAND_VALUE         32785
 #define _APS_NEXT_CONTROL_VALUE         1005
 #define _APS_NEXT_SYMED_VALUE           105
 #endif
